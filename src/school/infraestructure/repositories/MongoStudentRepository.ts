561e3e455eeb3bcff894893e3e1953a931f26bda
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-05-04 22:22:01 -0600

    mongo repositories

---
+++ src/school/infraestructure/repositories/MongoStudentRepository.ts
@@ -0,0 +1,166 @@
+import { Collection } from "mongodb";
+import { StudentInterface } from "../../domain/ports/StudentInterface";
+import { connect } from "../../../database/mongo";
+import { Student } from "../../domain/entities/Student";
+import { MongoSubjectRepository } from "./MongoSubjectRepository";
+import { MongoTutorRepository } from "./MongoTutorRepository";
+
+export class MongoStudentRepository implements StudentInterface {
+    private collection!: Collection | any;
+
+    private _subjectRepository: MongoSubjectRepository | null = null;
+    private _tutorRepository: MongoTutorRepository | null = null;
+
+    get subjectRepository(): MongoSubjectRepository {
+        if (!this._subjectRepository) {
+            this._subjectRepository = new MongoSubjectRepository();
+        }
+        return this._subjectRepository;
+    }
+
+    get tutorRepository(): MongoTutorRepository {
+        if (!this._tutorRepository) {
+            this._tutorRepository = new MongoTutorRepository();
+        }
+        return this._tutorRepository;
+    }
+
+    constructor() {
+        this.initializeCollection();
+    }
+
+    async create(student: Student): Promise<Student | null> {
+        try {
+            this.collection.insertOne(student);
+            return Promise.resolve(student);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async update(uuid: string, student: Student): Promise<Student | null> {
+        try {
+            let student_exist = await this.findByUUID(uuid);
+            if (student_exist) {
+                student.subjects = student_exist.subjects;
+                student.tutor = student_exist.tutor;
+                this.collection.updateOne({ uuid: uuid }, { $set: student });
+                return Promise.resolve(student);
+            } else {
+                return Promise.resolve(null);
+            }
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async delete(uuid: string): Promise<void> {
+        try {
+            this.collection.deleteOne({ uuid: uuid });
+            return Promise.resolve();
+        } catch (error) {
+            return Promise.resolve();
+        }
+    }
+
+    async findByUUID(uuid: string): Promise<Student | null> {
+        try {
+            const result = await this.collection.findOne({ uuid });
+            if (result) {
+                let subject = new Student(result.name, result.email, result.lastname);
+                subject.uuid = result.uuid;
+                subject.tutor = result.tutor;
+                subject.subjects = result.subjects;
+                return subject;
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async findAllStudentsByTutor(uuidTutor: string): Promise<Student[] | null> {
+        try {
+            const result = await this.collection.find({ tutor: uuidTutor }).toArray();
+            if (result) {
+                return result.map((element: any) => {
+                    let tutor = new Student(element.name, element.email, element.lastname);
+                    tutor.uuid = element.uuid;
+                    tutor.tutor = element.tutor;
+                    tutor.subjects = element.subjects;
+                    return tutor;
+                });
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async list(): Promise<Student[] | null> {
+        try {
+            const result = await this.collection.find().toArray();
+            if (result) {
+                return result.map((element: any) => {
+                    let tutor = new Student(element.name, element.email, element.lastname);
+                    tutor.tutor = element.tutor;
+                    tutor.subjects = element.subjects;
+                    tutor.uuid = element.uuid;
+                    return tutor;
+                });
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async addSubjectToStudent(uuidStudent: string, uuidSubject: string): Promise<Student | null> {
+        try {
+            let result = await this.findByUUID(uuidStudent);
+            if (result) {
+                await this.collection.updateOne({ uuid: uuidStudent }, { $addToSet: { subjects: uuidSubject } });
+                result = await this.findByUUID(uuidStudent);
+                if (result) {
+                    let student = new Student(result.name, result.email, result.lastname);
+                    student.uuid = result.uuid;
+                    student.tutor = result.tutor;
+                    student.subjects = result.subjects;
+                    let r = await this.subjectRepository.addStudentToSubject(uuidSubject, uuidStudent);
+                    console.log(r);
+                    return Promise.resolve(student);
+                }
+                return Promise.resolve(result);
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async addTutorToStudent(uuidStudent: string, uuidTutor: string): Promise<Student | null> {
+        try {
+            let result = await this.findByUUID(uuidStudent);
+            if (result) {
+                await this.collection.updateOne({ uuid: uuidStudent }, { $set: { tutor: uuidTutor } });
+                if (result) {
+                    let student = new Student(result.name, result.email, result.lastname);
+                    student.uuid = result.uuid;
+                    student.tutor = uuidTutor;
+                    student.subjects = result.subjects;
+                    let r = await this.tutorRepository.addStudentToTutor(uuidTutor, uuidStudent);
+                    console.log(r);
+                    return Promise.resolve(student);
+                }
+                return Promise.resolve(result);
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    private async initializeCollection(): Promise<void> {
+        this.collection = await connect("students");
+    }
+}
---
+++ src/school/infraestructure/repositories/MongoTutorRepository.ts
@@ -0,0 +1,102 @@
+import { Collection } from "mongodb";
+import { TutorInterface } from "../../domain/ports/TutorInterface";
+import { Tutor } from "../../domain/entities/Tutors";
+import { connect } from "../../../database/mongo";
+
+export class MongoTutorRepository implements TutorInterface {
+    private collection!: Collection | any;
+
+    constructor() {
+        this.initializeCollection();
+    }
+    // 9095a7f9-d517-4bd1-af62-247796d572dc
+    async addStudentToTutor(uuidTutor: string, uuidStudent: string): Promise<Tutor | null> {
+        try {
+            let result = await this.findByUUID(uuidTutor);
+            if (result) {
+                await this.collection.updateOne({ uuid: uuidTutor }, { $addToSet: { students: uuidStudent } });
+                result = await this.findByUUID(uuidTutor);
+                if (result) {
+                    let tutor = new Tutor(result.name, result.lastname, result.email);
+                    tutor.uuid = result.uuid;
+                    tutor.students = result.students;
+                    return Promise.resolve(tutor);
+                }
+                return Promise.resolve(result);
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            console.log(error);
+            return Promise.resolve(null);
+        }
+    }
+
+    async create(tutor: Tutor): Promise<Tutor | null> {
+        try {
+            this.collection.insertOne(tutor);
+            return Promise.resolve(tutor);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async update(uuid: string, tutor: Tutor): Promise<Tutor | null> {
+        try {
+            let tutor_exist = await this.findByUUID(uuid);
+            if (tutor_exist) {
+                tutor.students = tutor_exist.students;
+                this.collection.updateOne({ uuid: uuid }, { $set: tutor });
+                return Promise.resolve(tutor);
+            } else {
+                return Promise.resolve(null);
+            }
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async delete(uuid: string): Promise<void> {
+        try {
+            this.collection.deleteOne({ uuid: uuid });
+            return Promise.resolve();
+        } catch (error) {
+            return Promise.resolve();
+        }
+    }
+
+    async findByUUID(uuid: string): Promise<Tutor | null> {
+        try {
+            const result = await this.collection.findOne({ uuid });
+            if (result) {
+                let tutor = new Tutor(result.name, result.lastname, result.email);
+                tutor.uuid = result.uuid;
+                tutor.students = result.students;
+                return tutor;
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    async list(): Promise<Tutor[] | null> {
+        try {
+            const result = await this.collection.find().toArray();
+            if (result) {
+                return result.map((element: any) => {
+                    let tutor = new Tutor(element.name, element.lastname, element.email);
+                    tutor.uuid = element.uuid;
+                    tutor.students = element.students;
+                    return tutor;
+                });
+            }
+            return Promise.resolve(null);
+        } catch (error) {
+            return Promise.resolve(null);
+        }
+    }
+
+    private async initializeCollection(): Promise<void> {
+        this.collection = await connect("tutors");
+    }
+}
